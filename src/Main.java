1c966cad1c6a3b70b5fe39b0ce5a3f50c0939dfb
anton.shelepov <anton.shelepov@jetbrains.com>
2020-02-11 16:41:42 +0300

    minor

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         int minutes = Integer.parseInt(args[0]);
         StringBuilder output = new StringBuilder();
-        for (int i = 0; i < 100000; i++) {
+        for (int i = 0; i < 1000000; i++) {
             output.append("a");
         }
 
