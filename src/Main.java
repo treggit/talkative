6e76b84375bf3ea6edfb6585e267be7f953cc8db
anton.shelepov <anton.shelepov@jetbrains.com>
2020-02-11 14:30:08 +0300

    minor

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,9 @@ public class Main {
     }
 
     public static void main(String[] args) {
+        int minutes = Integer.parseInt(args[0]);
         long start = System.currentTimeMillis();
-        while (minutesPassed(start) < 10) {
+        while (minutesPassed(start) < minutes) {
             System.err.println("As build log can grow very large, let's ensure agent memory consumption is reasonably limited and build log is not stored in memory in its fullness. Caching it on disk will help.\n" +
                     "\n" +
                     "With this not fixed, agent can encounter outOfMemory error is the build log does not fit in memory and server goes offline");
