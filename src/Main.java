de05aa9a111bbdc1ed39d08ef842bd5d370dbcfe
anton.shelepov <anton.shelepov@jetbrains.com>
2020-04-29 09:12:44 +0300

    minor

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
 
     public static long minutesPassed(long start) {
@@ -9,17 +13,34 @@ public class Main {
     public static void main(String[] args) {
         int minutes = Integer.parseInt(args[0]);
         StringBuilder output = new StringBuilder();
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 1000; i++) {
             output.append("a");
         }
 
+        int threads = 8;
         long start = System.currentTimeMillis();
+        ExecutorService executor = Executors.newFixedThreadPool(threads);
+        for (int i = 0; i < threads; i++) {
+            int thread = i + 1;
+            executor.submit(() -> {
+                while (minutesPassed(start) < minutes) {
+                    System.err.println("Thread " + thread + " says: ");
+                    System.err.println(output);
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        break;
+                    }
+                }
+                System.out.println("Logging thread " + thread + " exited");
+            });
+        }
+
         while (minutesPassed(start) < minutes) {
-            System.err.println(output);
             try {
-                Thread.sleep(50);
+                Thread.sleep(1000);
             } catch (InterruptedException e) {
-                break;
+                e.printStackTrace();
             }
         }
     }
